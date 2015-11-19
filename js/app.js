cca20324ed74cd97c03d05cb244f47f4ba9f9033
Hai Nguyen <hai427@uw.edu>
2015-11-19 15:40:06 -0800

    completed lab, added eraser

--- js/app.js
+++ js/app.js
@@ -4,9 +4,50 @@
     See the README.md file for instructions
     https://github.com/drstearns/canvas-lab/blob/master/README.md
 */
+var eraser = false;
 
 document.addEventListener('DOMContentLoaded', function() {
     'use strict';
 
+    var canvas = document.querySelector('canvas');
+    var ctx = canvas.getContext('2d');
+
+    var mouseClicked = false;
+
+    document.addEventListener('mousedown', function(evt) {
+        ctx.beginPath();
+        var canvasY = evt.clientY - canvas.offsetTop;
+        var canvasX = evt.clientX - canvas.offsetLeft;
+        ctx.moveTo(canvasX, canvasY);
+        mouseClicked = true;
+    });
+
+    document.addEventListener('mousemove', function(evt) {
+        var canvasY = evt.clientY - canvas.offsetTop;
+        var canvasX = evt.clientX - canvas.offsetLeft;
+
+        var color = document.getElementById('line-color-inp').value;
+        ctx.strokeStyle = color;
+
+        var width = document.getElementById('stroke-width-inp').value;
+        ctx.lineWidth = width;
+
+        if(mouseClicked) {
+            if (eraser) {
+                ctx.clearRect(canvasX, canvasY, width, width);
+            } else {
+                ctx.lineTo(canvasX, canvasY);
+                ctx.stroke();
+            }
+        }
+    });
+
+    document.addEventListener('mouseup', function(evt) {
+        mouseClicked = false;
+    });
 
 }); //DOMContentLoaded
+
+function erase() {
+    eraser = !eraser;
+};
